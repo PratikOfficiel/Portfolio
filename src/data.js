d204cb8ab64225362fc0775b0e63842e5c7f3ae5
Pratik01-git <Pratik01-git>
2024-02-13 16:11:51 +0530

    project update

--- src/data.js
+++ src/data.js
@@ -1,15 +1,15 @@
-import IMG1 from './assets/portfolio1.jpg'
-import IMG2 from './assets/portfolio2.jpg'
-import IMG3 from './assets/portfolio3.jpg'
-import IMG4 from './assets/portfolio4.jpg'
+import IMG1 from './assets/portfolio1.png'
+import IMG2 from './assets/portfolio2.png'
+import IMG3 from './assets/portfolio3.png'
+import IMG4 from './assets/portfolio4.png'
 import IMG5 from './assets/portfolio5.png'
-import IMG6 from './assets/portfolio6.jpg'
+import IMG6 from './assets/portfolio6.png'
 
 const personalInfo = {
     name: "Pratik Soni",
     profession: 'Software Developer',
     institute: 'Indian Institute of Technology (BHU), Varanasi',
-    about: 'dhinka falana bakchodi abhi ke liye'
+    about: ''
 }
 
 const SKILLS = {
@@ -52,44 +52,44 @@ const projects = [
     {
         id:1,
         imgLink: IMG1,
-        title: "First Project",
-        githubLink: "www.github.com",
-        liveLink: "www.github.com"
+        title: "Comic Generator",
+        githubLink: "https://github.com/PratikOfficiel/ComicGenerator",
+        liveLink: "https://main--silver-swan-b7c197.netlify.app/"
     },
     {
         id:2,
         imgLink:IMG2,
-        title: "First Project",
-        githubLink: "www.github.com",
-        liveLink: "www.github.com"
+        title: "Notes On Blockchain",
+        githubLink: "https://github.com/PratikOfficiel/NotesOnBlockchain",
+        liveLink: "https://github.com/PratikOfficiel/NotesOnBlockchain"
     },
     {
         id:3,
         imgLink:IMG3,
-        title: "First Project",
-        githubLink: "www.github.com",
-        liveLink: "www.github.com"
+        title: "ContactBook",
+        githubLink: "https://github.com/PratikOfficiel/ContactBook",
+        liveLink: "https://vocal-sunflower-770480.netlify.app/"
     },
     {
         id:4,
         imgLink:IMG4,
-        title: "First Project",
-        githubLink: "www.github.com",
-        liveLink: "www.github.com"
+        title: "Random Quote",
+        githubLink: "https://github.com/PratikOfficiel/RandomQuoteGenerator",
+        liveLink: "https://master--comfy-syrniki-03b859.netlify.app/"
     },
     {
         id:5,
         imgLink:IMG5,
-        title: "First Project",
-        githubLink: "www.github.com",
+        title: "Nested Comments",
+        githubLink: "https://github.com/Pratik01-git/NComment",
         liveLink: "www.github.com"
     },
     {
         id:6,
         imgLink:IMG6,
-        title: "First Project",
-        githubLink: "www.github.com",
-        liveLink: "www.github.com"
+        title: "Portfolio Website",
+        githubLink: "https://github.com/PratikOfficiel/Portfolio",
+        liveLink: "https://stalwart-dolphin-440902.netlify.app/"
     },
 ];
 
