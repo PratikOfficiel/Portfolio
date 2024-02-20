b057189f615cae7fef7ca158b59e5faad8fcb056
Pratik01-git <Pratik01-git>
2024-02-20 20:40:58 +0530

    linkedin link update

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -5,7 +5,7 @@ import { GrTechnology } from "react-icons/gr";
 function HeaderSocials() {
   return (
     <div className='header_socials'>
-        <a href="https://www.linkedin.com/in/pratik-s-bb15521b2/" rel='noreferrer' target='_blank'><BsLinkedin/></a>
+        <a href="https://www.linkedin.com/in/spratik929/" rel='noreferrer' target='_blank'><BsLinkedin/></a>
         <a href="https://github.com/PratikOfficiel" rel='noreferrer' target='_blank'><FaGithub/></a>
         <a href="#skills"><GrTechnology /></a>
     </div>
